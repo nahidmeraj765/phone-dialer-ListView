fa72f0650a032bb3df8f83ca64b6d72bcae613a6
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-07-24 17:44:20 +0600

    listview added

--- lib/home.dart
+++ lib/home.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Phone Dialer"), 
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.phone),
+              trailing: Icon(Icons.delete), color: Colors.red,
+              title: Text("Nahid"),
+              subtitle: Text("01234567890"),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/home.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
